eecd108f047e8bd4d06a55b679beae866a329374
Drakon9927 <drakon9927@libero.it>
2024-04-16 21:24:08 +0200

    js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const nome = document.getElementById('nome');
+const nome = prompt("Inserisci il tuo nome");
 
-const password = document.getElementById('password');
+const cognome = prompt("Inserisci il tuo cognome");
 
+const colore = prompt("Inserisci il tuo colore preferito");
+
+const password = nome + cognome + colore + "#24";
+
+alert("La tua nuova password è: " + password);
